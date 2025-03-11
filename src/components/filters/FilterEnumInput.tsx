66f299ca56245def30b2cfd4bb42203f00b29a57
fl1k <vgolubovic97@gmail.com>
2025-03-11 10:34:09 +0100

    Implement initial FilterEnumInput

--- src/components/filters/FilterEnumInput.tsx
+++ src/components/filters/FilterEnumInput.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import * as React from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
 export interface FilterEnumInputProps<T extends string> {
   propertyName: string;
   value: T;
@@ -6,3 +17,34 @@ export interface FilterEnumInputProps<T extends string> {
   placeholder?: string;
   optionToString?: (option: T) => string;
 }
+
+const FilterEnumInput = <T extends string>({
+                                             propertyName,
+                                             value,
+                                             onChange,
+                                             options,
+                                             placeholder,
+                                             optionToString = (option: T) => option.toString(),
+                                           }: FilterEnumInputProps<T>) => {
+  return (
+    <div className="filter-input w-full">
+      <Select
+        value={value}
+        onValueChange={(newValue) => onChange(propertyName, newValue as T)}
+      >
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder={placeholder || `Select ${propertyName}`} />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option} value={option}>
+              {optionToString(option)}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default FilterEnumInput;
