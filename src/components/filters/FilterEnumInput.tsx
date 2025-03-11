19f6740dfc9ba20fddef07fd07fba568c7ea4b1c
fl1k <vgolubovic97@gmail.com>
2025-03-11 21:03:52 +0100

    Refactor formatting on FilterEnumInput

--- src/components/filters/FilterEnumInput.tsx
+++ src/components/filters/FilterEnumInput.tsx
@@ -19,44 +19,44 @@ export interface FilterEnumInputProps<TFilterKey, T extends string> {
 }
 
 const FilterEnumInput = <TFilterKey, T extends string>({
-                                                         propertyName,
-                                                         value,
-                                                         onChange,
-                                                         options,
-                                                         placeholder,
-                                                         optionToString = (option: T) => option.toString(),
-                                                       }: FilterEnumInputProps<TFilterKey, T>) => {
+  propertyName,
+  value,
+  onChange,
+  options,
+  placeholder,
+  optionToString = (option: T) => option.toString(),
+}: FilterEnumInputProps<TFilterKey, T>) => {
   return (
-      <div className="filter-input w-full">
-        <Select
-            value={value}
-            onValueChange={(newValue) => {
-              if (newValue === '_____CLEAR_____') {
-                onChange(propertyName, '' as T);
-              } else {
-                onChange(propertyName, newValue as T);
-              }
-            }}
-        >
-          <SelectTrigger className="w-full h-full">
-            <SelectValue placeholder={placeholder || `Select ${String(propertyName)}`} />
-          </SelectTrigger>
+    <div className="filter-input w-full">
+      <Select
+        value={value}
+        onValueChange={(newValue) => {
+          if (newValue === '_____CLEAR_____') {
+            onChange(propertyName, '' as T);
+          } else {
+            onChange(propertyName, newValue as T);
+          }
+        }}
+      >
+        <SelectTrigger className="w-full h-full">
+          <SelectValue
+            placeholder={placeholder || `Select ${String(propertyName)}`}
+          />
+        </SelectTrigger>
 
+        <SelectContent>
+          <SelectItem key="clear" value="_____CLEAR_____">
+            &nbsp;
+          </SelectItem>
 
-          <SelectContent>
-            <SelectItem key="clear" value="_____CLEAR_____">
-              &nbsp;
+          {options.map((option) => (
+            <SelectItem key={option} value={option}>
+              {optionToString(option)}
             </SelectItem>
-
-            {options.map((option) => (
-                <SelectItem key={option} value={option}>
-                  {optionToString(option)}
-                </SelectItem>
-            ))}
-
-          </SelectContent>
-        </Select>
-      </div>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
   );
 };
 
