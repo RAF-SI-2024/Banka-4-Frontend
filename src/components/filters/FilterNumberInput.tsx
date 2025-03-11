d503f2747f82518f143e2b506e777b683e7246ea
fl1k <vgolubovic97@gmail.com>
2025-03-11 10:35:24 +0100

    Refacotr FilterInputProps

--- src/components/filters/FilterNumberInput.tsx
+++ src/components/filters/FilterNumberInput.tsx
@@ -1,6 +1,21 @@
+'use client';
+
+import React from 'react';
+
 export interface FilterNumberInputProps {
   propertyName: string;
-  value: string;
-  onChange: (propertyName: string, newValue: string) => void;
+  value: number;
+  onChange: (propertyName: string, newValue: number) => void;
   placeholder?: string;
 }
+
+const FilterNumberInput: React.FC<FilterNumberInputProps> = ({
+  propertyName,
+  value,
+  onChange,
+  placeholder,
+}) => {
+  return null;
+};
+
+export default FilterNumberInput;
