c68b6f2bf175fa9cce92183b3ea5903bc987f57a
Marko Kocic <markokocic2000@gmail.com>
2025-03-13 21:24:21 +0100

    *: format

--- src/types/card.ts
+++ src/types/card.ts
@@ -1,14 +1,14 @@
 export interface Card {
-    cardNumber: string;
-    cardType: string;
-    cardName: string;
-    createdDate: string;
-    expiryDate: string;
-    accountNumber: string;
-    cvv: string;
-    limit: number;
-    status: string;
-    cardOwner: string;
+  cardNumber: string;
+  cardType: string;
+  cardName: string;
+  createdDate: string;
+  expiryDate: string;
+  accountNumber: string;
+  cvv: string;
+  limit: number;
+  status: string;
+  cardOwner: string;
 }
 
 export interface CardInfo {
